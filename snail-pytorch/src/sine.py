cd5d6348b3ab7a81c8626efc5aa8e1707d59415c
Erik Sorensen <sorensen.erik48@gmail.com>
2020-02-04 16:05:54 -0500

    Added packages

--- snail-pytorch/src/sine.py
+++ snail-pytorch/src/sine.py
@@ -1,13 +1,16 @@
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
 
 class SineWaveTask:
     def __init__(self):
         self.a = np.random.uniform(0.1, 5.0)
         self.b = np.random.uniform(0, 2*np.pi)
         self.train_x = None
-        
+
     def f(self, x):
         return self.a * np.sin(x + self.b)
-        
+
     def training_set(self, size=10, force_new=False):
         if self.train_x is None and not force_new:
             self.train_x = np.random.uniform(-5, 5, size)
@@ -18,12 +21,12 @@ class SineWaveTask:
             x = np.random.uniform(-5, 5, size)
         y = self.f(x)
         return torch.Tensor(x), torch.Tensor(y)
-    
+
     def test_set(self, size=50):
         x = np.linspace(-5, 5, size)
         y = self.f(x)
         return torch.Tensor(x), torch.Tensor(y)
-    
+
     def plot(self, *args, **kwargs):
         x, y = self.test_set(size=100)
         return plt.plot(x.numpy(), y.numpy(), *args, **kwargs)
