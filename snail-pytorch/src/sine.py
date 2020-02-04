8eccf124e27f66e5459727ab0de8910e228e2ae4
Caleb Lyon <31669372+CalebL98@users.noreply.github.com>
2020-02-04 16:00:56 -0500

    Generates sine curves

---
+++ snail-pytorch/src/sine.py
@@ -0,0 +1,29 @@
+
+class SineWaveTask:
+    def __init__(self):
+        self.a = np.random.uniform(0.1, 5.0)
+        self.b = np.random.uniform(0, 2*np.pi)
+        self.train_x = None
+        
+    def f(self, x):
+        return self.a * np.sin(x + self.b)
+        
+    def training_set(self, size=10, force_new=False):
+        if self.train_x is None and not force_new:
+            self.train_x = np.random.uniform(-5, 5, size)
+            x = self.train_x
+        elif not force_new:
+            x = self.train_x
+        else:
+            x = np.random.uniform(-5, 5, size)
+        y = self.f(x)
+        return torch.Tensor(x), torch.Tensor(y)
+    
+    def test_set(self, size=50):
+        x = np.linspace(-5, 5, size)
+        y = self.f(x)
+        return torch.Tensor(x), torch.Tensor(y)
+    
+    def plot(self, *args, **kwargs):
+        x, y = self.test_set(size=100)
+        return plt.plot(x.numpy(), y.numpy(), *args, **kwargs)
